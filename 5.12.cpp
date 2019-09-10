ea1874075344fdbdac7ef2b2690624b2bd3e1ad3
Nikkone7979 <54699761+Nikkone7979@users.noreply.github.com>
2019-09-09 22:11:00 -0400

    Update 5.12.cpp

--- 5.12.cpp
+++ 5.12.cpp
@@ -1,15 +1,38 @@
-//5.12 - Write a program that uses a for statement tocalculate and print the product of the odd integers from 1 to 15
 #include <iostream>
 #include <string>
 using namespace std;
 
+
+
+void space(int n) {
+	int i = 0;
+	int arr[10];
+	while (n > 0)
+	{
+		arr[i] = n % 10;
+		n = n / 10;
+		i++;
+	}
+	for (i = i - 1; i >= 0; i--)
+	{
+		cout << arr[i] << " ";
+	}
+
+}
+
 int main() {
-	int i;
-	int index{ 1 };
-	for (i = 1; i <= 15; i = i + 2) {
-		index = index * i;
-		cout << "Odd integeder: " << i << " " << endl;
-		cout << "Product of previous odd integers: " << index << endl;
-	};
+	int a{ 0 };
+	int b{ 0 };
+	int quotient{ 0 };
+	int remainder{ 0 };
+	cout << "Enter two integers: " << endl;
+	cin >> a;
+	cin >> b;
+	quotient = (a / b);
+	remainder = (a % b);
+	cout << "The quotient of these two integers is: " << quotient << endl;
+	cout << "The remainder of these two integers after division is: " << remainder << endl;
+	space(rand()*a);
+	cout << endl;
 	system("pause");
-};
+}	
